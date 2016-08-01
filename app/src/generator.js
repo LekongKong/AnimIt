560f2c43ff972fcfcdb0fd9fed75f2661a8d7357
陈雨童 <chenyutong@baixing.com>
2016-08-01 15:34:30 +0800

    尝试优化generator的性能。

--- app/src/generator.js
+++ app/src/generator.js
@@ -1,42 +1,81 @@
 /**
  * Created by b1ncer on 16/7/4.
  */
-const cal = (start, end, duration, ease, delta) => {
-    return ease(delta / duration) * (end - start) + start;
+const calc = (start, end, duration, ease, begin, cb) => {
+    cb(ease((new Date().getTime() - begin) / duration) * (end - start) + start);
 };
 
 export default function* generator (from, to, duration, ease) {
     const begin = new Date().getTime();
+    let start, end, res, fns = [];
+    if (typeof from == 'number') {
+        start = from;
+        end = to;
+
+        fns.push(
+            () => {
+                res = 'inited';
+            }
+        );
+
+        fns.push(
+            () => {
+                calc(start, end, duration, ease, begin, value => {
+                    res = value;
+                });
+            }
+        );
+
+    } else if (Array.isArray(from)) {
+
+        fns.push(
+            () => {
+                res = [];
+            }
+        );
+
+        for (let i = 0, j = from.length; i < j; i++) {
+            start = from[i];
+            end = to[i];
+
+            fns.push(
+                () => {
+                    calc(start, end, duration, ease, begin, value => {
+                        res.push(value);
+                    });
+                }
+            );
+        }
+    } else {
+
+        fns.push(
+            () => {
+                res = {};
+            }
+        );
+
+        for (const key in from) {
+            start = from[key];
+            end = to[key];
+
+            fns.push(
+                () => {
+                    calc(start, end, duration, ease, begin, value => {
+                        res[key] = value;
+                    });
+                }
+            );
+        }
+    }
     while (true) {
-        const now = new Date().getTime();
-        const delta = now - begin;
-        if (delta >= duration) {
+        if (new Date().getTime() - begin >= duration) {
             return to;
         }
-        let start, end, res;
-        if (typeof from == 'number') {
-            start = from;
-            end = to;
-            res = cal(start, end, duration, ease, delta);
-        } else if (Array.isArray(from)) {
-            res = [];
-            for (let i = 0, j = from.length; i < j; i++) {
-                start = from[i];
-                end = to[i];
-                if (typeof start == 'number' && typeof end == 'number') {
-                    res[i] = cal(start, end, duration, ease, delta);
-                }
-            }
-        } else {
-            res = {};
-            for (const key in from) {
-                start = from[key];
-                end = to[key];
-                if (typeof start == 'number' && typeof end == 'number') {
-                    res[key] = cal(start, end, duration, ease, delta);
-                }
-            }
-        }
+
+        fns.forEach(fn => {
+            fn();
+        });
+
         yield res;
     }
 }
